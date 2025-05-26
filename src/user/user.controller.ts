07f25acde078e9f9e5bac2b8525c20cf11a34e23
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2025-05-26 18:21:07 +0600

    created users service to get all user and specific user by id

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  findAllUser() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findSingleUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id);
+  }
+
+  // @Param('id'): Extracts the id from the URL parameters
+  //ParseIntPipe: Converts the id (which comes in as a string from the URL) into a number. If it's not a valid number, Nest will throw a 400 Bad Request automatically.
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  // get all users
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+}
